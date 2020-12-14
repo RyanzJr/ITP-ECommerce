4028a8ed7cf55c6bf54440c7d03d3cd4a5ea5857
RyanzJr <ryantanhuiyang45aips11@gmail.com>
2020-12-14 17:34:04 +0800

    Login Page and Routing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { LoginPage } from '../login/login.page';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  
 })
+
+
+
+
 export class HomePage {
 
-  constructor() {}
+  
+
+
+
+  constructor() { 
+  }
+
+
 
 }
