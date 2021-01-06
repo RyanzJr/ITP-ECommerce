3d1ec611c90734c64b6c3032ed534c9bb3e8d218
zzzrot <44426344+zzzrot@users.noreply.github.com>
2021-01-06 17:48:15 +0800

    profile retrieve & update

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProfileService, User } from 'src/app/services/profile.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  user: User = {
+    Username: '',
+    Email: '',
+    Gender:'',
+    Contact:'',
+    Company:'',
+
+  };
+
+  constructor( private activatedRoute: ActivatedRoute, private ideaService: ProfileService,
+    private toastCtrl: ToastController, private router: Router) { }
 
   ngOnInit() {
+  }
+  ionViewWillEnter() {
+    let id = "IjaDqfZ6tqRlu97rLCLQ";
+   // let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.ideaService.getUser(id).subscribe(user => {
+        this.user = user;
+      });
+    }
+  }
+
+  updateProfile() {
+    this.ideaService.updateUser(this.user).then(() => {
+      this.router.navigateByUrl('/profile');
+      this.showToast('Idea updated');
+    }, err => {
+      this.showToast('There was a problem updating your profile :(');
+    });
+  }
+
+  showToast(msg) {
+    this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    }).then(toast => toast.present());
   }
 
 }
