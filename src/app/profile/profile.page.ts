6582cd2618a55632559d437bfaf6d3a4a5fde140
zzzrot <44426344+zzzrot@users.noreply.github.com>
2021-01-28 13:46:16 +0800

    able to upload and retrieve and display images

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProfileService, User } from 'src/app/services/profile.service';
 import { ToastController } from '@ionic/angular';
+import { AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import { AngularFireStorage} from '@angular/fire/storage';
+//import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-profile',
@@ -10,21 +13,24 @@ import { ToastController } from '@ionic/angular';
 })
 export class ProfilePage implements OnInit {
 
+  selectedFile: any;
+  url: any;
+
   user: User = {
     Username: '',
     Gender:'',
     Contact:'',
     Company:'',
-
+    Image:'',
   };
 
   constructor( private activatedRoute: ActivatedRoute, private ideaService: ProfileService,
-    private toastCtrl: ToastController, private router: Router) { }
+    private toastCtrl: ToastController, private router: Router, private store:AngularFireStorage) { }
 
   ngOnInit() {
   }
   ionViewWillEnter() {
-    let id = "IjaDqfZ6tqRlu97rLCLQ";
+    let id = "KCl0vEOjUny0wH3Pi8y7";
    // let id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) {
       this.ideaService.getUser(id).subscribe(user => {
@@ -33,13 +39,30 @@ export class ProfilePage implements OnInit {
     }
   }
 
-  updateProfile() {
+  chooseFile(event){
+    this.selectedFile = event.target.files
+  }
+
+  async UploadFile (id, file): Promise<any>{
+    if(file && file.length){
+      try{
+      const task = await this.store.ref('images').child(id).put(file[0])
+      return this.store.ref(`images/${id}`).getDownloadURL().toPromise();
+    } catch (error){
+      console.log(error);}
+    }
+    
+  }
+
+  async updateProfile() {
     this.ideaService.updateUser(this.user).then(() => {
       this.router.navigateByUrl('/profile');
       this.showToast('Idea updated');
     }, err => {
       this.showToast('There was a problem updating your profile :(');
     });
+    this.user.Image = await this.UploadFile(this.user.id,this.selectedFile);
+    console.log(this.user.Image );
   }
 
   showToast(msg) {
@@ -50,3 +73,4 @@ export class ProfilePage implements OnInit {
   }
 
 }
+
